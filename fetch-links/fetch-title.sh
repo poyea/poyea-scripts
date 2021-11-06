f2c02bbe7f0bdb3a00e9803a2788d62fde3ab5f1
John Law <johnlaw.po@gmail.com>
2021-11-07 01:44:20 +0800

    Add fetch links

---
+++ fetch-links/fetch-title.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+while read -r URL; do
+    echo -n "$URL → "
+    wget -q -O - "$URL" | tr "\n" " " | sed 's|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||'
+    echo
+    sleep 0.5
+done
