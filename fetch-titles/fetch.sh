935ee541120536430d4f3b67bfb3104bd73326c5
John Law <johnlaw.po@gmail.com>
2021-12-10 02:16:16 +0800

    use sh, rename to fetch

--- fetch-titles/fetch.sh
+++ fetch-titles/fetch.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/env sh
+#!/bin/sh
+
+set -e
+
 while read -r URL; do
     echo -n "$URL → "
     wget -q -O - "$URL" | tr "\n" " " | sed 's|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||'
