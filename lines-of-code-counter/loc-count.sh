6531d40bf1ad13fa3f4694aa97cdc39b71bb8d7c
John Law <johnlaw.po@gmail.com>
2022-03-12 23:38:10 +0000

    add LOC counter

---
+++ lines-of-code-counter/loc-count.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+cd "$(dirname "$0")"
+
+if [ ! -z $1 ] 
+then 
+    case $1 in
+    "cpp")
+        echo "Counting $1..."
+        find . \( -name \*.cpp \
+                -o -name \*.c \
+                -o -name \*.h \
+                -o -name \*.hpp \
+                -o -name \*.cc \
+                -o -name \*.CPP \
+                -o -name \*.C \
+                -o -name \*.cxx \
+                -o -name \*.c++ \
+                \) \
+                -not -path ./thirdparty/\* \
+                -not -path ./build/\* \
+                | xargs wc -l
+        ;;
+    *)
+        echo "Hmm... What did you type???"
+        ;;
+    esac
+else
+    echo "Counting every possible thing..."
+    find . -type f | xargs wc -l
+fi
