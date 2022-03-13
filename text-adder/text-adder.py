7d2e206a750c7153d5cf65f42d2ec0eb1dd2fe44
John Law <johnlaw.po@gmail.com>
2022-03-13 22:41:39 +0000

    fix: default help

--- text-adder/text-adder.py
+++ text-adder/text-adder.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import re
 from pathlib import Path
-
+import argparse
+import sys
 
 def prepend_text(text_loc, directories, pattern, verbose):
     for d in directories:
@@ -15,7 +16,6 @@ def prepend_text(text_loc, directories, pattern, verbose):
 
 
 if __name__ == "__main__":
-    import argparse
 
     parser = argparse.ArgumentParser(
         description="Add text to your files.", epilog="Use this when you are tired."
@@ -39,9 +39,11 @@ if __name__ == "__main__":
     )
     parser.add_argument("-v", help="verbose mode", action="store_true")
 
-    args = parser.parse_args()
+    try:
+        args = parser.parse_args()
+        prepend_text(args.f[0], args.d, args.e[0], args.v)
+    except:
+        parser.print_help()
+        sys.exit(0)
 
-    assert len(args.e) == 1
-    assert len(args.f) == 1
 
-    prepend_text(args.f[0], args.d, args.e[0], args.v)
