0fff2d99391dba531d01df32eced85b90b26b8a1
John Law <johnlaw.po@gmail.com>
2022-04-20 10:49:34 +0000

    improve styles

--- fetch-titles/fetch-titles.sh
+++ fetch-titles/fetch-titles.sh
@@ -5,7 +5,7 @@ set -e
 while read -r URL
 do
     echo -n "$URL → "
-    wget -q -O - "$URL" | tr "\n" " " | sed 's|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||'
+    wget -q -O - "$URL" | tr "\n" " " | sed "s|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||"
     echo
     sleep 0.5
 done
